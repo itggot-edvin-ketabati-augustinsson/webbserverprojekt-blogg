eb12014ddd864d8354630166f01485baafcc3017
itggot-edvin-ketabati-augustinsson <edvket@gmail.com>
2019-02-17 20:32:33 +0100

    Created empty DB, copied register + login functionality fr auth-proj.

---
+++ Blogg/app.rb
@@ -0,0 +1,48 @@
+require 'slim'
+require 'sinatra'
+require 'sqlite3'
+require 'bcrypt'
+
+enable :sessions
+
+get('/') do
+    slim(:slim)
+end
+
+post('/login') do
+    db = SQLite3::Database.new('blogg.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT Password FROM users WHERE Username =(?)", params["name"]) 
+    not_password = result[0]["Password"]
+    if BCrypt::Password.new(not_password) == params["pass"]
+        session[:loggedin] = true
+        redirect('/welcome')
+    else
+        redirect('/lolno')
+    end
+end
+
+get('/welcome') do
+    if session[:loggedin] == true
+        slim(:welcome)
+    else
+        redirect('/')
+    end
+end
+
+post('/logout') do
+    session.destroy
+    redirect('/')
+end
+
+get('/register') do
+    slim(:register)
+end
+
+post('/create') do
+    name = params["name"]
+    password = BCrypt::Password.create(params["pass"])
+    db = SQLite3::Database.new('blogg.db')
+    db.execute("INSERT INTO users(Username, Password) VALUES('#{name}','#{password}')")
+    redirect('/')
+end
